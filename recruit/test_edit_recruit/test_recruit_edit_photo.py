dd9f5279751bb6193469c991d7331419267221c1
bequite <valentin8dev@gmail.com>
2019-11-21 21:23:17 +0300

    upd: Test Cases Skills, Photo

--- recruit/test_edit_recruit/test_recruit_edit_photo.py
+++ recruit/test_edit_recruit/test_recruit_edit_photo.py
@@ -1,18 +1,8 @@
-import os
-import sys
-
-import django
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-path = os.path.expanduser(BASE_DIR)
-if path not in sys.path:
-    sys.path.insert(0, path)
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BelHardCRM.settings")
-django.setup()
-
+from client.edit.utility import time_it
 from recruit.models import Recruiter
 
 
@@ -28,6 +18,35 @@ class RecruiterEditPhotoTests(TestCase):
                                                   self.TEST_USER_PASSWORD)
         self.client_inst = Recruiter.objects.create(recruiter=self.test_user)
         self.url = reverse('recruit_edit_photo')
+
+    @time_it
+    def test_page_open_user(self):
+        self.client.login(username=self.TEST_USER_USERNAME, password=self.TEST_USER_PASSWORD)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'recruit/edit_pages/recruit_photo.html')
+
+    @time_it
+    def test_GET_nUnD(self):  # no user no data
+        response = self.client.get(path=self.url)
+        self.assertEqual(response.context['form'].data, {})
+
+    @time_it
+    def test_POST_nUnD(self):  # no user no data
+        response = self.client.post(path=self.url, data={'form': ''})
+        self.assertEqual(response.status_code, 302)
+
+    @time_it
+    def test_GET_wUnD(self):  # with user no data
+        self.client.login(username=self.TEST_USER_USERNAME, password=self.TEST_USER_PASSWORD)
+        response = self.client.get(path=self.url)
+        self.assertEqual(response.context['form'].data, {})
+
+    @time_it
+    def test_POST_wUnD(self):  # with user no data
+        self.client.login(username=self.TEST_USER_USERNAME, password=self.TEST_USER_PASSWORD)
+        response = self.client.post(path=self.url, data={})
+        self.assertEqual(response.status_code, 302)
 
 
 if __name__ == "__main__":
